43e43e9b5bd6c427420d3956f8c45e216f4513ab
루피 <ucna1984@gmail.com>
2025-05-20 12:40:34 +0900

    Initial Commit

---
+++ src/main/java/com/javago/triplog/TriplogApplication.java
@@ -0,0 +1,13 @@
+package com.javago.triplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriplogApplication.class, args);
+	}
+
+}
