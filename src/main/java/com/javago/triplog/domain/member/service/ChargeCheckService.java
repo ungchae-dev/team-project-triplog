dd8a0266e0e8b07f51a3f3cd2c0f97c0cb0aedc1
devkmh <kmhdev420@gmail.com>
2025-05-22 12:42:27 +0900

    rename ChargeService.java > ChargeCheckService.java

--- src/main/java/com/javago/triplog/domain/member/service/ChargeCheckService.java
+++ src/main/java/com/javago/triplog/domain/member/service/ChargeCheckService.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 @Service
 @RequiredArgsConstructor
-public class ChargeService {
+public class ChargeCheckService {
 
     @Value("${portone.api-key}")
     private String apiKey;
