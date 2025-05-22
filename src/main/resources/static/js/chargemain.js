d9f4e267a82fd40711e15d9372fea588f4b2254c
devkmh <kmhdev420@gmail.com>
2025-05-22 17:13:10 +0900

    chargemain.js modify (2)

--- src/main/resources/static/js/chargemain.js
+++ src/main/resources/static/js/chargemain.js
@@ -9,7 +9,7 @@ async function requestPay() {
     const paymentId = crypto.randomUUID();
 
     const payment = await PortOne.requestPayment({
-        storeId: "tosstest",
+        storeId: "store-4e640aa5-588e-43a9-acf5-ebaeff70b074",
         channelKey: "channel-key-8c3e9cfb-a553-471c-bea5-148c3270fea1",
         paymentId: paymentId,
         orderName: item.name,
@@ -20,6 +20,9 @@ async function requestPay() {
             item: item.id
         }
     });
+
+    console.log("✅ 결제된 paymentId:", payment.paymentId);
+    console.log("✅ 전체 응답 내용:", payment);
 
     if (payment.code !== undefined) {
         alert("결제 실패: " + payment.message);
@@ -38,7 +41,7 @@ async function requestPay() {
         const result = await response.json();
         if (result.status === "PAID") {
             alert(`결제 성공! 도토리 ${result.charged}개가 충전되었습니다.`);
-            location.href = "/mypage";
+            location.href = "";
         }
     } else {
         alert("결제 검증 실패");
