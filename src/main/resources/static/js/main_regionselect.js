bdd79e9c4acb903361e2d1341c57639fc431cbbb
DongYoungShin <aszx082101@gmail.com>
2025-05-26 18:19:21 +0900

    0526_dy_3rd_mainpage_1820

--- src/main/resources/static/js/main_regionselect.js
+++ src/main/resources/static/js/main_regionselect.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 행사 API 호출 함수
     function fetchEventsByAreaCode(areaCode) {
-        fetch(`https://apis.data.go.kr/B551011/KorService1/searchFestival1?numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&eventStartDate=20250101&areaCode=${areaCode}&serviceKey=WE8zMSHqcnIgkNM8%2BArCN71r3exZEj%2FG4cPNj9NW8bb4quc1fmi2oxTpPF1C1aWmDl%2FXeAWBqQO6XMjJlShceg%3D%3D`)
+        fetch(`https://apis.data.go.kr/B551011/KorService2/searchFestival2?numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&eventStartDate=20250101&areaCode=${areaCode}&serviceKey=WE8zMSHqcnIgkNM8%2BArCN71r3exZEj%2FG4cPNj9NW8bb4quc1fmi2oxTpPF1C1aWmDl%2FXeAWBqQO6XMjJlShceg%3D%3D&_type=json`)
             .then(res => res.json())
             .then(data => {
                 eventData = data.response.body.items.item || [];
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 관광지 API 호출 함수
     function fetchToursByAreaCode(areaCode) {
-        fetch(`http://apis.data.go.kr/B551011/KorService1/areaBasedList1?numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&ServiceKey=WE8zMSHqcnIgkNM8%2BArCN71r3exZEj%2FG4cPNj9NW8bb4quc1fmi2oxTpPF1C1aWmDl%2FXeAWBqQO6XMjJlShceg%3D%3D&listYN=Y&arrange=A&contentTypeId=12&areaCode=${areaCode}&sigunguCode=&cat1=&cat2=&cat3=`)
+        fetch(`http://apis.data.go.kr/B551011/KorService1/areaBasedList1?numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&ServiceKey=WE8zMSHqcnIgkNM8%2BArCN71r3exZEj%2FG4cPNj9NW8bb4quc1fmi2oxTpPF1C1aWmDl%2FXeAWBqQO6XMjJlShceg%3D%3D&listYN=Y&arrange=A&contentTypeId=12&areaCode=${areaCode}&sigunguCode=&cat1=&cat2=&cat3=&_type=json`)
             .then(res => res.json())
             .then(data => {
                 tourData = data.response.body.items.item || [];
@@ -90,7 +90,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 맛집 API 호출 함수
     function fetchFoodsByAreaCode(areaCode) {
-        fetch(`http://apis.data.go.kr/B551011/KorService1/areaBasedList1?numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&ServiceKey=WE8zMSHqcnIgkNM8%2BArCN71r3exZEj%2FG4cPNj9NW8bb4quc1fmi2oxTpPF1C1aWmDl%2FXeAWBqQO6XMjJlShceg%3D%3D&listYN=Y&arrange=A&contentTypeId=39&areaCode=${areaCode}&sigunguCode=&cat1=&cat2=&cat3=`)
+        fetch(`http://apis.data.go.kr/B551011/KorService1/areaBasedList1?numOfRows=12&pageNo=1&MobileOS=ETC&MobileApp=AppTest&ServiceKey=WE8zMSHqcnIgkNM8%2BArCN71r3exZEj%2FG4cPNj9NW8bb4quc1fmi2oxTpPF1C1aWmDl%2FXeAWBqQO6XMjJlShceg%3D%3D&listYN=Y&arrange=A&contentTypeId=39&areaCode=${areaCode}&sigunguCode=&cat1=&cat2=&cat3=&_type=json`)
             .then(res => res.json())
             .then(data => {
                 foodData = data.response.body.items.item || [];
@@ -102,6 +102,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 행사 정보 렌더링
     function renderEventList() {
+        if (!eventData) return; // 또는 기본 빈 배열 할당
         const start = (currentPage - 1) * eventsPerPage;
         const end = start + eventsPerPage;
         const currentEvents = eventData.slice(start, end);
@@ -130,6 +131,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 관광지 정보 렌더링
     function renderTourList() {
+        if (!tourData) return;
         const tourstart = (currentTourPage - 1) * tourPerPage;
         const tourend = tourstart + tourPerPage;
         const currentTour = tourData.slice(tourstart, tourend);
@@ -158,6 +160,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 맛집 정보 렌더링
     function renderFoodList() {
+        if (!foodData) return;
         const foodstart = (currentFoodPage - 1) * foodPerPage;
         const foodend = foodstart + foodPerPage;
         const currentFood = foodData.slice(foodstart, foodend);
