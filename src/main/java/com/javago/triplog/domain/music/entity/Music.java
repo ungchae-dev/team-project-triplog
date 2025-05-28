6159b477015f9dedfed205d0e14011214a4508f9
devkmh <kmhdev420@gmail.com>
2025-05-28 16:44:10 +0900

    feature Music entity

---
+++ src/main/java/com/javago/triplog/domain/music/entity/Music.java
@@ -0,0 +1,34 @@
+package com.javago.triplog.domain.music.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "music")
+@Getter
+@Setter
+@ToString
+public class Music {
+
+    @Id
+    @Column(name = "music_id")
+    private Long musicId;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title; // 음악 제목
+
+    @Column(name = "artist", nullable = false, length = 50)
+    private String artist; // 가수 이름
+
+    @Column(name = "album", length = 50)
+    private String album; // 앨범명 (nullable)
+
+    @Lob // CLOB 타입으로 음악 파일 경로를 저장
+    @Column(name = "music_file", nullable = false)
+    private String musicFile; // 음악 파일 경로
+
+    @Column(name = "price", nullable = false)
+    private int price; // 음원 가격 한 곡당 평균 700원, 1곡당 도토리 10개
+}
