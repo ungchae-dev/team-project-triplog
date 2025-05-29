75556cdb6a2b745981ad0e8d81fbe628264459d8
Yoo-sungmin <seongmin4342@gmail.com>
2025-05-29 16:35:19 +0900

    a

--- src/main/java/com/javago/triplog/domain/post/controller/PostViewController.java
+++ src/main/java/com/javago/triplog/domain/post/controller/PostViewController.java
@@ -3,6 +3,8 @@ package com.javago.triplog.domain.post.controller;
 import com.javago.triplog.domain.post.dto.PostListResponse;
 import com.javago.triplog.domain.post.entity.Post;
 import com.javago.triplog.domain.post.service.PostService;
+import com.javago.triplog.domain.post_hashtag_people.entity.Post_Hashtag_people;
+import com.javago.triplog.domain.post_hashtag_people.service.PostHashtagPeopleService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,11 +20,13 @@ import java.util.List;
 public class PostViewController {
 
     private final PostService postService;
+    private final PostHashtagPeopleService postHashtagPeopleService;
 
     // 게시판 글 목록
     @GetMapping("/blog/post/list")
     public String list(Model model) {
         List<PostListResponse> postList = postService.findPostList();
+        
         model.addAttribute("postList", postList);
         return "post/list";
     }
@@ -31,7 +35,9 @@ public class PostViewController {
     @GetMapping("/blog/post/{id}")
     public String getPost(@PathVariable("id") Long id, Model model) {
         Post post = postService.findById(id);
+        List<Post_Hashtag_people> hashtagList = postHashtagPeopleService.findHashtagList(id);
         model.addAttribute("post", post);
+        model.addAttribute("hashtagList", hashtagList);
         return "post/detail";
     }
 
