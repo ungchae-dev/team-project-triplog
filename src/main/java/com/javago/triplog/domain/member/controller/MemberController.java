937a133a35edaa2681913ff9d70c920e659f8bdb
ungchae-dev <ucnadev3@gmail.com>
2025-05-30 10:00:52 +0900

    chore: 회원가입,로그인 URL 매핑 /member/login으로 변경

--- src/main/java/com/javago/triplog/domain/member/controller/MemberController.java
+++ src/main/java/com/javago/triplog/domain/member/controller/MemberController.java
@@ -8,13 +8,18 @@ import com.javago.triplog.domain.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/members")
+@RequestMapping("/member")
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
+    // 회원가입·로그인 페이지로 매핑 
+    @GetMapping("/login")
+    public String login() {
+        return "member/register_login"; // templates/member/register_login.html
+    }
     
 
 }
