20b70117364618f1245a3c45afd71b441255d95b
ungchae-dev <ucnadev3@gmail.com>
2025-05-30 12:41:45 +0900

    chore: MemberController 자잘한 수정

--- src/main/java/com/javago/triplog/domain/member/controller/MemberController.java
+++ src/main/java/com/javago/triplog/domain/member/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.javago.triplog.domain.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.javago.triplog.domain.member.dto.MemberFormDto;
 import com.javago.triplog.domain.member.service.MemberService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,11 +17,11 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // 회원가입·로그인 페이지로 매핑 
-    @GetMapping("/login")
-    public String login() {
+    // 회원가입·로그인 페이지로 이동
+    @GetMapping(value = "/login")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/register_login"; // templates/member/register_login.html
     }
-    
 
 }
