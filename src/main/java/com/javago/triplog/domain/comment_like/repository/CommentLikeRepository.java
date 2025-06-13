cb8490c53f41c61f694861ebe77f9f956571a91e
Yoo-sungmin <seongmin4342@gmail.com>
2025-06-13 17:09:07 +0900

    fix comment and add comment like

--- src/main/java/com/javago/triplog/domain/comment_like/repository/CommentLikeRepository.java
+++ src/main/java/com/javago/triplog/domain/comment_like/repository/CommentLikeRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.javago.triplog.domain.comment_like.entity.Comment_Like;
+import com.javago.triplog.domain.comments.entity.Comments;
+import com.javago.triplog.domain.member.entity.Member;
 
 @Repository("CommentLikeRepository")
 public interface CommentLikeRepository extends JpaRepository<Comment_Like, Long> {
+
+    Comment_Like findByMemberAndComment(Member member, Comments comment);
     
 }
