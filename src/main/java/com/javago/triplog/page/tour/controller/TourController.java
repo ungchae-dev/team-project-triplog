8e5a7ddeea92ff444a9dc7188b93a7246f1f5ea0
DongYoungShin <aszx082101@gmail.com>
2025-06-16 16:30:50 +0900

    0616_pagedebug(search, tour)_1630

--- src/main/java/com/javago/triplog/page/tour/controller/TourController.java
+++ src/main/java/com/javago/triplog/page/tour/controller/TourController.java
@@ -1,20 +1,54 @@
 package com.javago.triplog.page.tour.controller;
 
-import lombok.RequiredArgsConstructor;
+import com.javago.triplog.domain.member.entity.CustomUserDetails;
+import com.javago.triplog.domain.member.entity.Member;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TourController {
 
     @GetMapping("/tour")
-    public String showTourPage(@RequestParam String areaCode,
-                               @RequestParam String category,
+    public String showTourPage(@RequestParam(defaultValue = "1") String areaCode,
+                               @RequestParam(defaultValue = "event") String category,
                                @RequestParam(defaultValue = "1") int page,
-                               Model model) {
+                               Model model, Authentication authentication) {
+
+        // 로그인 상태 확인 및 모델에 추가
+        boolean isLoggedIn = (authentication != null && authentication.isAuthenticated() && !authentication.getName().equals("anonymousUser"));
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("isLoggedIn", isLoggedIn);
+
+        if (auth != null && auth.isAuthenticated()
+                && auth.getPrincipal() instanceof CustomUserDetails customUserDetails ) {
+
+            Member member = customUserDetails.getMember();
+            model.addAttribute("nickname", member.getNickname());
+            model.addAttribute("role", member.getRole().name()); // USER 또는 ADMIN
+        }
+
+        if (isLoggedIn && authentication != null) {
+            // 로그인된 사용자 정보 추가
+            String memberId = authentication.getName();
+            model.addAttribute("username", memberId);
+
+            // 로그인된 사용자 로깅
+            System.out.println("로그인된 사용자: " + memberId);
+            System.out.println(" - 권한: " + authentication.getAuthorities());
+
+        } else {
+            // 비로그인 상태 로깅
+            System.out.println("비로그인 상태");
+        }
+
+        // 지역 정보 모델에 추가
+
+
         model.addAttribute("areaCode", areaCode);
         model.addAttribute("category", category);
         model.addAttribute("page", page);
